a40cc94551dcbe66e585bf88e766fcbee983d138
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2023-10-07 15:29:14 -0400

    Add Chakra UI Wrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { ChakraProvider } from '@chakra-ui/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -10,8 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+        <body>
+        <ChakraProvider>
+          {children}
+          </ChakraProvider>  
+          </body>
     </html>
   )
 }
