5d87d127cc84ee76f1cb2daf7a7b9b2a61dc262d
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2023-10-07 15:53:22 -0400

    Add Pages router

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../app/page.module.css'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 export default function Home() {
