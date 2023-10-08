569723d44df04a74ddba97326634ad88d6b45c1e
Nada El-Falou <nadafalou2001@gmail.com>
2023-10-08 15:42:43 -0400

    fixed error in featuredData

--- src/app/data/featuredData.js
+++ src/app/data/featuredData.js
@@ -1,39 +1,37 @@
 export const FeturedData = {
-    [
-        {
-            name: "Spooky tour",
-            date: "01-01-2034",
-            planets: [
-                {
-                    destinations: [
-                        "Saturn",
-                    ],
-                    hotel: "Ringmaster's Retreat",
-                }, {
-                    destinations: [
-                        "Uranus",
-                    ],
-                    hotel: "Crystal Chateau",
-                }
-            ]
-        },
-        {
-            name: "Spooky tour 2",
-            date: "06-10-2024",
-            planets: [
-                {
-                    destinations: [
-                        "Jupiter",
-                        "The Great Red Spot",
-                    ],
-                    hotel: "Storm Seeker Lodge",
-                }, {
-                    destinations: [
-                        "Neptune",
-                    ],
-                    hotel: "Aqua Voyager Resort",
-                }
-            ]
-        }
-    ]
+    "01-01-2034": {
+        name: "Spooky tour",
+        date: "01-01-2034",
+        planets: [
+            {
+                destinations: [
+                    "Saturn",
+                ],
+                hotel: "Ringmaster's Retreat",
+            }, {
+                destinations: [
+                    "Uranus",
+                ],
+                hotel: "Crystal Chateau",
+            }
+        ]
+    },
+    "06-10-2024": {
+        name: "Spooky tour 2",
+        date: "06-10-2024",
+        planets: [
+            {
+                destinations: [
+                    "Jupiter",
+                    "The Great Red Spot",
+                ],
+                hotel: "Storm Seeker Lodge",
+            }, {
+                destinations: [
+                    "Neptune",
+                ],
+                hotel: "Aqua Voyager Resort",
+            }
+        ]
+    }
 }
