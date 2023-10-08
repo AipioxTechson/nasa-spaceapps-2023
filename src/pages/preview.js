6d62d1bb4d069a0853ef139f2e98f0b501ef2316
Niveditha Kani <nivedithakani@gmail.com>
2023-10-08 10:28:01 -0400

    Fix button

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -1,12 +1,24 @@
 import { Box, Button, Text, Heading, Center, Spacer } from "@chakra-ui/react";
 import "@fontsource-variable/baloo-bhai-2";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	useDisclosure,
+} from "@chakra-ui/react";
 
 export default function Preview() {
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	return (
 		<main>
 			<Box bgImage="url('/olympusmons.jpg')" h="100vh">
 				<Center bg="brand.300" p={5}>
-					<Button mr={10}>Back</Button>
+					<Button>Back</Button>
+					<Spacer />
 					<Box align="center">
 						<Heading color="brand.200" fontWeight="light">
 							Olympus Mons
@@ -16,9 +28,29 @@ export default function Preview() {
 						</Text>
 					</Box>
 					<Spacer />
-
-					<Button mr={5}>Tips to visit</Button>
-					<Button>Next destination</Button>
+                    <Button onClick={onOpen}>Tips for visiting</Button>
+					<Modal isOpen={isOpen} onClose={onClose}>
+						<ModalOverlay />
+						<ModalContent>
+							<ModalHeader>Tips for visiting</ModalHeader>
+							<ModalCloseButton />
+							<ModalBody>
+								Tips go here
+							</ModalBody>
+							<ModalFooter>
+								<Button
+									colorScheme="blue"
+									mr={3}
+									onClick={onClose}
+								>
+									Close
+								</Button>
+								<Button variant="ghost">
+									Secondary Action
+								</Button>
+							</ModalFooter>
+						</ModalContent>
+					</Modal>
 				</Center>
 			</Box>
 		</main>
