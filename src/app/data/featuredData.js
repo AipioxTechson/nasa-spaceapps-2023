7deb0f5c553096f99c01bc5f4929940e8823b224
Nada El-Falou <nadafalou2001@gmail.com>
2023-10-08 15:34:47 -0400

    slight format change in dutured tour dataset

--- src/app/data/featuredData.js
+++ src/app/data/featuredData.js
@@ -1,33 +1,39 @@
 export const FeturedData = {
-    "01-01-2034": {
-        planets: [
-            {
-                destinations: [
-                    "Saturn",
-                ],
-                hotel: "Ringmaster's Retreat",
-            }, {
-                destinations: [
-                    "Uranus",
-                ],
-                hotel: "Crystal Chateau",
-            }
-        ]
-    },
-    "06-10-2024": {
-        planets: [
-            {
-                destinations: [
-                    "Jupiter",
-                    "The Great Red Spot",
-                ],
-                hotel: "Storm Seeker Lodge",
-            }, {
-                destinations: [
-                    "Neptune",
-                ],
-                hotel: "Aqua Voyager Resort",
-            }
-        ]
-    }
+    [
+        {
+            name: "Spooky tour",
+            date: "01-01-2034",
+            planets: [
+                {
+                    destinations: [
+                        "Saturn",
+                    ],
+                    hotel: "Ringmaster's Retreat",
+                }, {
+                    destinations: [
+                        "Uranus",
+                    ],
+                    hotel: "Crystal Chateau",
+                }
+            ]
+        },
+        {
+            name: "Spooky tour 2",
+            date: "06-10-2024",
+            planets: [
+                {
+                    destinations: [
+                        "Jupiter",
+                        "The Great Red Spot",
+                    ],
+                    hotel: "Storm Seeker Lodge",
+                }, {
+                    destinations: [
+                        "Neptune",
+                    ],
+                    hotel: "Aqua Voyager Resort",
+                }
+            ]
+        }
+    ]
 }
