ed94cada88f0c439dd9b207eaa08e22a50af68eb
Niveditha Kani <nivedithakani@gmail.com>
2023-10-08 16:54:00 -0400

    Add itinerary dests

--- src/app/Components/Travel.js
+++ src/app/Components/Travel.js
@@ -1,17 +1,21 @@
 import React from "react";
-import {
-	Box,
-	Text,
-} from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
-function Travel() {
+function Travel({ type, time }) {
 	return (
-		<Box w="80vw" borderRadius="lg" bg="brand.700" align="center" mb={3} p={2}>
+		<Box
+			w="80vw"
+			borderRadius="lg"
+			bg="brand.700"
+			align="center"
+			mb={3}
+			p={2}
+		>
 			<Text color="brand.800" fontWeight="bold" fontSize="xs">
 				TRAVEL
 			</Text>
-			<Text color="brand.800">Travel time: 100 hours</Text>
-			<Text color="brand.800">Transportation type: Warp drive</Text>
+			<Text color="brand.800">Travel time: {time}</Text>
+			<Text color="brand.800">Transportation type: {type}</Text>
 			<Text color="brand.800">
 				Instructions: A tour guide will pick you up from your hotel.
 			</Text>
--- src/app/data/featuredData.js
+++ src/app/data/featuredData.js
@@ -1,76 +1,63 @@
-export const FeturedData = {
-    "01-01-2034": {
-        name: "Spooky tour",
-        date: "01-01-2034",
-        planets: [
-            {
-                destinations: [
-                    "Saturn",
-                ],
-                hotel: "Ringmaster's Retreat",
-            }, {
-                destinations: [
-                    "Uranus",
-                ],
-                hotel: "Crystal Chateau",
-            }
-        ]
-    },
-    "06-10-2034": {
-        name: "Spooky tour 2",
-        date: "06-10-2034",
-        planets: [
-            {
-                destinations: [
-                    "Jupiter",
-                    "The Great Red Spot",
-                ],
-                hotel: "Storm Seeker Lodge",
-            }, {
-                destinations: [
-                    "Neptune",
-                ],
-                hotel: "Aqua Voyager Resort",
-            }
-        ]
-    },
-    "07-10-2034": {
-        name: "Spooky tour 3",
-        date: "07-10-2034",
-        planets: [
-            {
-                destinations: [
-                    "Mercury",
-                ],
-                hotel: "Solar Flare Retreat",
-            }, {
-                destinations: [
-                    "Venus",
-                    "Maat Mons",
-                    "Artemis Chasma",
-                ],
-                hotel: "Cloud 9 Oasis",
-            }
-        ]
-    },
-    "19-09-2035": {
-        name: "Spooky tour 4",
-        date: "19-09-2035",
-        planets: [
-            {
-                destinations: [
-                    "Mercury",
-                ],
-                hotel: "Solar Flare Retreat",
-            }, {
-                destinations: [
-                    "Mars",
-                    "Aureum Chaos",
-                    "Noctis Labyrinthus",
-                    "Olympus Mons",
-                ],
-                hotel: "Red Rock Resort",
-            }
-        ]
-    }
-}
+export const featuredData = {
+	"01-01-2034": {
+		name: "Spooky tour",
+		date: "01-01-2034",
+		planets: [
+			{
+				destinations: ["Saturn", "Rings", "Titan"],
+				hotel: "Ringmaster's Retreat",
+			},
+			{
+				destinations: ["Uranus"],
+				hotel: "Crystal Chateau",
+			},
+		],
+	},
+	"06-10-2034": {
+		name: "Spooky tour 2",
+		date: "06-10-2034",
+		planets: [
+			{
+				destinations: ["Jupiter", "The Great Red Spot"],
+				hotel: "Storm Seeker Lodge",
+			},
+			{
+				destinations: ["Neptune"],
+				hotel: "Aqua Voyager Resort",
+			},
+		],
+	},
+	"07-10-2034": {
+		name: "Spooky tour 3",
+		date: "07-10-2034",
+		planets: [
+			{
+				destinations: ["Mercury"],
+				hotel: "Solar Flare Retreat",
+			},
+			{
+				destinations: ["Venus", "Maat Mons", "Artemis Chasma"],
+				hotel: "Cloud 9 Oasis",
+			},
+		],
+	},
+	"19-09-2035": {
+		name: "Spooky tour 4",
+		date: "19-09-2035",
+		planets: [
+			{
+				destinations: ["Mercury"],
+				hotel: "Solar Flare Retreat",
+			},
+			{
+				destinations: [
+					"Mars",
+					"Aureum Chaos",
+					"Noctis Labyrinthus",
+					"Olympus Mons",
+				],
+				hotel: "Red Rock Resort",
+			},
+		],
+	},
+};
