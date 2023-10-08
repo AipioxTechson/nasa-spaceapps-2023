e394f17923f347a5367c8010ce565a532fa2f947
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2023-10-08 15:23:19 -0400

    Clean up tour data

--- src/app/data/tourData.js
+++ src/app/data/tourData.js
@@ -18,124 +18,122 @@ export const TourData = {
     ], 
     moonData : [
       {
-                lat: 20,
-                lng: 20,
-                name: "Test",
-                alt: 0.5,
-                image: "2k_mars.jpeg",
-                size: 10
-              }
-            ]
-
-          },
-          Mars: {
-            globeImage: "mars_texture.jpg",
-    destinations: [{
-      name: "Aureum Chaos",
-      summary: "Rollerblading through the golden chaos",
-      lat: (-4.4 ),
-      lng: (333),
-    },
-    {
-      name: "Olympus Mons",
-      summary: "Yup",
+        lat: 20,
+        lng: 20,
+        name: "Test",
+        alt: 0.5,
+        image: "2k_mars.jpeg",
+        size: 10
+      }
+    ]
+  },
+  Mars: {
+    globeImage: "mars_texture.jpg",
+    destinations: [
+      {
+        name: "Aureum Chaos",
+        summary: "Rollerblading through the golden chaos",
+        lat: (-4.4 ),
+        lng: (333),
+      },
+      {
+        name: "Olympus Mons",
+        summary: "Yup",
+        lat: (18.8),
+        lng: (-132),
+      },
+      {
+        name: "Noctis Labyrinthus",
+        summary: "The Great Martian Maze",
+        lat: (-6.5),
+        lng: (260),
+      }
+      ]
+  },
+  Venus: {
+    globeImage: "venus_shaded.jpg",
+    destinations: [
+      {
+        name: "Maat Mons",
+        summary: "Visit the Roman Goddess’ Fiery Volcano",
+        lat: (-30),
+        lng: (135),
+      },
+      {
+      name: "Artemis Chasma",
+      summary: "Walk along Venus' biggest corona",
       lat: (18.8),
       lng: (-132),
-    },{
-      name: "Noctis Labyrinthus",
-      summary: "The Great Martian Maze",
-      lat: (-6.5),
-      lng: (260),
-    }
-  ]
+      }
+    ]
   },
-             
-          Venus: {
-      
-              globeImage: "venus_shaded.jpg",
-              destinations: [{
-                name: "Maat Mons",
-                summary: "Visit the Roman Goddess’ Fiery Volcano",
-                lat: (-30),
-                lng: (135),
-              },
-              {
-                name: "Artemis Chasma",
-                summary: "Walk along Venus' biggest corona",
-                lat: (18.8),
-                lng: (-132),
-              }
-            ]
-              },
-           
-        
-          Jupiter: {
-            globeImage: "2k_mars.jpeg",
-            destinations: [
-              {
-                name: "The Great Red Spot",
-                summary: "A storm bigger than earth",
-                lat: (Math.random() - 0.5) * 180,
-                lng: (Math.random() - 0.5) * 360,
-              },
-            ],
-          },
-          Mercury: {
-            globeImage: "2k_mars.jpeg",
-            destinations: [
-              {
-                name: "Mercury",
-                summary: "A storm bigger than earth",
-                lat: (Math.random() - 0.5) * 180,
-                lng: (Math.random() - 0.5) * 360,
-              },
-            ],
-          },
-          Saturn: {
-            globeImage: "2k_mars.jpeg",
-            destinations: [
-              {
-                name: "Saturn",
-                summary: "A storm bigger than earth",
-                lat: (Math.random() - 0.5) * 180,
-                lng: (Math.random() - 0.5) * 360,
-              },
-            ],
-          },
-          Uranus: {
-            globeImage: "2k_mars.jpeg",
-            destinations: [
-              {
-                name: "Uranus",
-                summary: "A storm bigger than earth",
-                lat: (Math.random() - 0.5) * 180,
-                lng: (Math.random() - 0.5) * 360,
-              },
-            ],
-          },
-          Neptune: {
-            globeImage: "2k_mars.jpeg",
-            destinations: [
-              {
-                name: "Neptune",
-                summary: "A storm bigger than earth",
-                lat: (Math.random() - 0.5) * 180,
-                lng: (Math.random() - 0.5) * 360,
-              },
-            ],
-          },
-          Pluto: {
-            globeImage: "2k_mars.jpeg",
-            destinations: [
-              {
-                name: "Pluto",
-                summary: "A storm bigger than earth",
-                lat: (Math.random() - 0.5) * 180,
-                lng: (Math.random() - 0.5) * 360,
-              },
-            ],
-          },
-        };     
+  Jupiter: {
+    globeImage: "2k_mars.jpeg",
+    destinations: [
+      {
+        name: "The Great Red Spot",
+        summary: "A storm bigger than earth",
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+      },
+    ],
+  },
+  Mercury: {
+    globeImage: "2k_mars.jpeg",
+    destinations: [
+      {
+        name: "Mercury",
+        summary: "A storm bigger than earth",
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+      },
+    ],
+  },
+  Saturn: {
+    globeImage: "2k_mars.jpeg",
+    destinations: [
+      {
+        name: "Saturn",
+        summary: "A storm bigger than earth",
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+      },
+    ],
+  },
+  Uranus: {
+    globeImage: "2k_mars.jpeg",
+    destinations: [
+      {
+        name: "Uranus",
+        summary: "A storm bigger than earth",
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+      },
+    ],
+  },
+  Neptune: {
+    globeImage: "2k_mars.jpeg",
+    destinations: [
+      {
+        name: "Neptune",
+        summary: "A storm bigger than earth",
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+      },
+    ],
+  },
+  Pluto: {
+    globeImage: "2k_mars.jpeg",
+    destinations: [
+      {
+        name: "Pluto",
+        summary: "A storm bigger than earth",
+        lat: (Math.random() - 0.5) * 180,
+        lng: (Math.random() - 0.5) * 360,
+      },
+    ],
+  },
+};     
     
     
 
