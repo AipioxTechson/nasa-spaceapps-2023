ceb268f545e9015c65e5f01bbb8e55588181a3bd
Nada El-Falou <nadafalou2001@gmail.com>
2023-10-08 15:31:21 -0400

    created features tour and hotel datasets

---
+++ src/app/data/featuredData.js
@@ -0,0 +1,33 @@
+export const FeturedData = {
+    "01-01-2034": {
+        planets: [
+            {
+                destinations: [
+                    "Saturn",
+                ],
+                hotel: "Ringmaster's Retreat",
+            }, {
+                destinations: [
+                    "Uranus",
+                ],
+                hotel: "Crystal Chateau",
+            }
+        ]
+    },
+    "06-10-2024": {
+        planets: [
+            {
+                destinations: [
+                    "Jupiter",
+                    "The Great Red Spot",
+                ],
+                hotel: "Storm Seeker Lodge",
+            }, {
+                destinations: [
+                    "Neptune",
+                ],
+                hotel: "Aqua Voyager Resort",
+            }
+        ]
+    }
+}
---
+++ src/app/data/hotelData.js
@@ -0,0 +1,22 @@
+export const HotelData = {
+    "Solar Flare Retreat": {
+        planet: "Mercury",
+        decription: "A futuristic hotel buried deep within the crust of Mercury to escape the extreme surface temperatures. Guests can explore the surface via advanced heat-resistant suits and enjoy a breathtaking view of the Sun."
+    },
+    "Storm Seeker Lodge": {
+        planet: "Jupiter",
+        decription: "An orbital hotel that hovers above Jupiter's turbulent clouds. It provides panoramic views of the Great Red Spot and other storms. Guests can enjoy simulated gravity and extreme weather experiences."
+    },
+    "Ringmaster's Retreat": {
+        planet: "Saturn",
+        description: "A luxurious space station-style hotel positioned within Saturn's magnificent rings. Visitors can dine with a view of the ring system and even take guided excursions to explore the icy ring particles."
+    },
+    "Crystal Chateau": {
+        planet: "Uranus",
+        description: "A glass-domed hotel on Uranus's moon Miranda, offering a breathtaking view of the planet's striking blue-green atmosphere and the unique terrain of Miranda's ice canyons."
+    },
+    "Aqua Voyager Resort": {
+        planet: "Neptune",
+        description: "A partially submerged hotel floating in Neptune's deep, frigid oceans. Guests can observe the mysterious marine life through large, reinforced glass walls while staying warm and dry."
+    }
+}
