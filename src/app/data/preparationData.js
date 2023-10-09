1d4fddee67c0afa1cb37030d89d2cbb9f61d7997
kunalkishanmehta <92482128+kunalkishanmehta@users.noreply.github.com>
2023-10-08 22:10:36 -0400

    preparation data

---
+++ src/app/data/preparationData.js
@@ -0,0 +1,22 @@
+export const preparationData = {
+	prepare: [
+		{
+			
+			a: " Read books and articles on how space travel can affect your body. Make sure you and your children know how space can affect your physical and mental health.",
+		},
+		{
+			a: "Before purchasing your tickets and starting the 4 month training, ride on a roller coaster and experience zero gravity yourself. Space travel can be hard, so make sure you know whether you can do this or not"
+            ,
+			
+		},
+		{
+			
+			a: "Space suits are uncomfortable. Underneath the spacesuit, you have to wear a Liquid Cooling and Ventilation Undergarment (LCVU) which can feel uncomfortable. Space suits are heavy. While on some planets, you may not feel it; on others, you can definitely feel the weight of the suit. Read up on spacesuit design and prepare yourself on how it feels.",
+		},
+		{
+			
+			a: "Food is dried and preserved with chemicals which may taste artificial and different. Make sure to read the ingredients list on the backside of the food packets if you have any allergies.",
+		},
+		
+	],
+};
